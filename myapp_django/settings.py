f990806867432f14df98efd4c2e6fcc032833126
naranujo <naraujo@udesa.edu.ar>
2023-03-15 21:04:14 -0300

    Sexto commit

--- myapp_django/settings.py
+++ myapp_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-&hy1solue174mwib2xkn_3m7q(q45e3z6)k8q6kckk4xvj&4om
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", "http://127.0.0.1:8000", "https://myapp-dajngo.herokuapp.com"]
 
 
 # Application definition
